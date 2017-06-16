38d23ce3a549de2c8c1d7845595f765374c6d505
Ata S.Mohammadi <ata.smohammadi@opencampus.net>
2017-06-16 14:32:17 +0200

    Initial Release

---
+++ checkboxGroup.js
@@ -0,0 +1,89 @@
+/**
+ * Checkbox group
+ * ataomega@gmail.com
+ * www.atasmohammadi.net
+ * version 1.0
+ */
+import React, {Component, PropTypes} from "react";
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+var { width, height } = Dimensions.get('window');
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default class CheckboxGroup extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pageWidth: Dimensions.get('window').width,
+      pageHeight: Dimensions.get('window').height,
+      selected: []
+    };
+  }
+
+  getNewDimensions(event){
+        var pageHeight = event.nativeEvent.layout.height
+        var pageWidth = event.nativeEvent.layout.width
+        this.setState({
+            pageHeight, pageWidth
+        })
+    }
+
+  _onSelect = (item) => {
+    var selected = this.state.selected
+    if(selected.indexOf(item) == -1){
+      selected.push(item)
+      this.setState({
+        selected: selected
+      })
+    } else {
+      selected = selected.filter(i => i != item)
+      this.setState({
+        selected: selected
+      })
+    }
+    this.props.callback(selected)
+  }
+
+  _isSelected = (item) => {
+    const selected = this.state.selected
+    if(selected.indexOf(item) == -1){
+      return false
+    }
+    return true
+  }
+
+  render(){
+    const { checkboxes, iconColor, iconSize, labelStyle, checkedIcon, uncheckedIcon } = this.props;
+
+    return(
+      <View
+        onLayout={(evt)=>{this.getNewDimensions(evt)}}
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          padding: 5
+        }}
+      >
+        {checkboxes.map((checkbox, index)=>{
+          <TouchableOpacity
+            key={index}
+            style={{ flexDirection: 'row' }}
+            onPress={()=>{
+              this._onSelect(checkbox.value)
+            }}
+          >
+            {this._isSelected(checkbox.value) ?
+              <Icon name={checkedIcon} color={iconColor} size={iconSize}/>
+              : <Icon name={uncheckedIcon} color={iconColor} size={iconSize}/>
+            }
+            <Text style={labelStyle}>{checkbox.label}</Text>
+          </TouchableOpacity>
+        })}
+      </View>
+    );
+  }
+}
